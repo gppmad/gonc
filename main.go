d6f37af64f1fb5084d55d0f22930f03bebbedb82
Giuseppe Maddiona <giuseppe.maddiona@gmail.com>
2025-03-22 11:53:52 +0100

    Move tcp client impl in another folder

--- main.go
+++ main.go
@@ -1,65 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"os"
+
+	tcp_client "github.com/gppmad/gonc/tcp_client"
 )
-
-type TcpClient struct {
-	Input  io.Reader
-	Output io.Writer
-	Conn   net.Conn
-}
-
-func NewTcpClient(conn net.Conn, input io.Reader, output io.Writer) *TcpClient {
-	if input == nil {
-		input = os.Stdin
-	}
-
-	if output == nil {
-		output = os.Stdout
-	}
-
-	return &TcpClient{Conn: conn, Input: input, Output: output}
-}
-
-// Before call this server initialize the connection with Connect()
-func (c *TcpClient) Start() error {
-	if c.Conn == nil {
-		return errors.New("connect to the target before initialize a new connection")
-	}
-
-	errChan := make(chan error, 1)
-
-	// Read from the connection.
-	go func() {
-		_, err := io.Copy(c.Output, c.Conn)
-		if err != nil {
-			errChan <- err
-			return
-		}
-		errChan <- nil
-	}()
-
-	_, err := io.Copy(c.Conn, c.Input)
-	if err != nil {
-		return errors.New("error writing in the connection")
-	}
-
-	if err := <-errChan; err != nil {
-		return errors.New("error reading from the connection: " + err.Error())
-	}
-	return nil
-}
-
-// Close the connection
-func (c *TcpClient) Close() error {
-	return c.Conn.Close()
-}
 
 func main() {
 
@@ -73,7 +20,7 @@ func main() {
 		log.Fatal("Error during connection: ", err)
 	}
 
-	tcpClient := NewTcpClient(conn, nil, nil)
+	tcpClient := tcp_client.NewTcpClient(conn, nil, nil)
 	fmt.Println("Connected to a TCP Server")
 
 	// Start the connection
--- tcp_client/tcp_client_test.go
+++ tcp_client/tcp_client_test.go
@@ -1,4 +1,4 @@
-package main
+package tcp_client
 
 import (
 	"bytes"
